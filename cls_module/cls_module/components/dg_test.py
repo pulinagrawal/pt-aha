8376b22491c16f0e00d8360c6524b4d5128c60a5
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2021-12-17 15:14:45 +1100

    Update DG test

--- cls_module/cls_module/components/dg_test.py
+++ cls_module/cls_module/components/dg_test.py
@@ -3,25 +3,26 @@
 import torch
 
 from cls_module.components.dg import DG
-from cls_module.components.dg_stub import DGStub
 
 config = {
     "inhibition_decay": 0.95,
     "knockout_rate": 0.25,
     "init_scale": 10.0,
-    "num_units": 225,
-    "sparsity": 10
+    "num_units": 200,
+    "sparsity": 10,
+    "use_stub": False
 }
 
-x = torch.rand(5, 121, 5, 5)
+# rule of thumb
+# num_units = batch_size * sparsity
+# control using sparsity and inhibition decay
+
+x = torch.rand(20, 121, 5, 5)
 
 torch.set_printoptions(threshold=5000)
 
 dg = DG(x.shape, config)
 out = dg(x)
-
-# dg = DGStub(x.shape, config)
-# out = dg(x)
 
 # Verify sparsity
 test_out = torch.sum(out, dim=1)
@@ -37,3 +38,8 @@ torch.testing.assert_allclose(test_out, expected_out)
 test_out, _ = torch.max(out, dim=1)
 expected_out = torch.ones_like(test_out)
 torch.testing.assert_allclose(test_out, expected_out)
+
+dg_overlap = dg.compute_overlap(out)
+
+print('Overlap (per sample) =', dg_overlap)
+print('Overlap (sum) =', dg_overlap.sum().item())
